b6e57904f9d6c645c3a5eb31944b3eb528a581a5
wznpp1 <wznpp1@hotmail.com>
2020-08-21 20:01:34 +0800

    Update sshset.sh

--- sshset.sh
+++ sshset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get -y update
-apt-get -y install dos2unix
+apt-get -y install tofrodos
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 service sshd restart
 echo root:lzhrwwc1 | chpasswd
-curl https://raw.githubusercontent.com/wznpp1/azure/master/get.ubuntu.sh|dos2unix >/root/get.ubuntu.sh
+curl https://raw.githubusercontent.com/wznpp1/azure/master/get.ubuntu.sh|todos >/root/get.ubuntu.sh
