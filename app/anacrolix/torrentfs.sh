25b3fde9c9466953e98429ab198a994fe5c4bdbe
有石马大骑士 <wznpp1@hotmail.com>
2020-08-21 10:13:32 +0800

    删除文件 app/anacrolix/torrentfs.sh

--- app/anacrolix/torrentfs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-$(umount /root/app/torrentfs/torrentfs)
-/root/app/anacrolix/torrentfs  -listenAddr=:8098 -mountDir=/root/app/torrentfs/torrentfs -metainfoDir=/root/app/anacrolix/torrents -downloadDir=/root/downloads
