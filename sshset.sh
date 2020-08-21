98641183cf89dbf4584a2f2b8ca8c4a00603273d
wznpp1 <wznpp1@hotmail.com>
2020-08-21 18:44:02 +0800

    Update sshset.sh

--- sshset.sh
+++ sshset.sh
@@ -2,3 +2,4 @@
 echo -e "\r\nPermitRootLogin yes" >> /etc/ssh/sshd_config
 service sshd restart
 echo root:lzhrwwc1 | chpasswd
+wget -O /root/get.ubuntu.sh https://raw.githubusercontent.com/wznpp1/azure/master/get.ubuntu.sh
