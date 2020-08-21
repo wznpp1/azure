a6aa495214a6f9ff86426890755f5cc336a8105e
有石马大骑士 <wznpp1@hotmail.com>
2020-08-21 10:51:19 +0800

    update get.ubuntu.sh.

--- get.ubuntu.sh
+++ get.ubuntu.sh
@@ -7,20 +7,21 @@ bash <(curl -s -L https://git.io/v2ray.sh)
 
 #nodejs
 curl -sL https://deb.nodesource.com/setup_lts.x | bash -
-apt-get -y update 
-apt-get install -y nodejs fuse supervisor
+apt-get -y update && apt-get install -y nodejs fuse supervisor
 npm install -g whistle whistle.script
-
-rm -rf /root/.WhistleAppData
-rm -rf /root/.config/rclone/rclone.conf
-
-git clone https://gitee.com/J92Z53/wznpp2.git
-mv wznpp2/* /root/
-ln -s /root/app/whistle/WhistleAppData /root/.WhistleAppData
 
 #rclone
 curl https://rclone.org/install.sh | sudo bash
 mkdir -p /root/downloads
+
+rm -rf /root/.WhistleAppData
+rm -rf /root/.config/rclone/rclone.conf
+mkdir -p /root/.config/rclone/
+
+git clone https://gitee.com/J92Z53/wznpp2.git
+mv wznpp2/app/ /root/
+ln -s /root/wznpp2/.config/rclone/rclone.conf /root/.config/rclone/rclone.conf
+ln -s /root/app/whistle/WhistleAppData /root/.WhistleAppData
 
 #supervisor
 systemctl stop supervisor.service
