bf3380269c788c50919f7a533088b5e3b201b54f
wznpp1 <wznpp1@hotmail.com>
2020-08-21 18:58:05 +0800

    Update sshset.sh

--- sshset.sh
+++ sshset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update && apt install dos2unix
-echo -e "\r\nPermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "\r\nPermitRootLogin yes" >> /etc/ssh/sshd_config
 service sshd restart
 echo root:lzhrwwc1 | chpasswd
 curl https://raw.githubusercontent.com/wznpp1/azure/master/get.ubuntu.sh|dos2unix >/root/get.ubuntu.sh
