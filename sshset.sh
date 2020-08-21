0a17fda9a62161bd2c09ae2de052a7172d568300
wznpp1 <wznpp1@hotmail.com>
2020-08-21 18:02:43 +0800

    Update sshset.sh

--- sshset.sh
+++ sshset.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-echo -e "\r\nPermitRootLogin yes">> /etc/ssh/sshd_config
+echo -e "\r\nPermitRootLogin yes" >> /etc/ssh/sshd_config
 service sshd restart
-
 echo root:lzhrwwc1 | chpasswd
